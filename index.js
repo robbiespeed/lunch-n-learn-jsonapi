a5cb2a0b01da96b4bd401a71675eaed8faf2f43d
Robbie Speed <robert.y.speed@gmail.com>
2016-06-30 02:44:31 -0300

    added default date

--- index.js
+++ index.js
@@ -15,15 +15,36 @@ const models = {
     posts: { link: 'post', inverse: 'author', isArray: true }
   },
   post: {
-    message: { type: String }, 
+    message: { type: String },
+    date: { type: Date },
     
     // One-to-many relationship of post author to user posts.
     author: { link: 'user', inverse: 'posts' }
   }
 };
 
+const transforms = {
+  post: [
+    (context, record, update) => {
+      const method = context.request.method;
+
+      if (method === 'create') {
+        record.date = new Date();
+        return record;
+      }
+
+      return update || null;
+    },
+    (context, record) => {
+      record.date = record.date && record.date.toLocaleString();
+      return record;
+    }
+  ]
+};
+
 const store = fortune(models, {
-  adapter: [ nedbAdapter, { dbPath: 'data/example' } ]
+  adapter: [ nedbAdapter, { dbPath: 'data/example' } ],
+  transforms
 });
 
 const listener = fortune.net.http(store, {
